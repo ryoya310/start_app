48226d0dde06b793d5fef7e956aef6d9723cfe33
Ryoya <109121569+ryoya310@users.noreply.github.com>
2023-04-19 19:04:25 +0900

    0.0.3

--- .batch/start.bat
+++ .batch/start.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set WORK_DIR=C:\work\start_app
+set WORK_DIR=C:\work\project\project.jp
 set PYTHON_APP=python
 
 echo --------------------
@@ -13,6 +13,12 @@ if %errorlevel% neq 0 (
     echo Error: Failed to start Docker.
     exit /b %errorlevel%
 )
+
+echo Waiting for Docker to start...
+:WAIT_DOCKER
+timeout /t 5 > NUL
+docker ps > NUL 2>&1
+if %errorlevel% neq 0 goto WAIT_DOCKER
 
 echo --------------------
 echo --- Python Start ---
--- .batch/start.sh
+++ .batch/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WORK_DIR=/work/start_app
+WORK_DIR=/work/project/project.jp
 PYTHON_APP=python
 
 echo --------------------
@@ -14,11 +14,14 @@ if [ $? -ne 0 ]; then
     exit $?
 fi
 
+echo Waiting for Docker to start...
+while ! docker ps > /dev/null 2>&1; do sleep 5; done
+
 echo --------------------
 echo --- Python Start ---
 echo --------------------
 
-cd $WORK_DIR/$PYTHON_APP
+cd "$WORK_DIR/$PYTHON_APP"
 python manage.py runserver 127.0.0.1:8310
 if [ $? -ne 0 ]; then
     echo Error: Failed to start Python app.
